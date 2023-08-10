c357e6407395dd4dd1b80fb318cc86a7db9b8a83
Firoz-Boss <141584252+Firoz-Boss@users.noreply.github.com>
2023-08-10 19:16:22 +0600

    Add files via upload

--- firoz.py
+++ firoz.py
@@ -440,8 +440,8 @@ class Main:
 			Public()
 		if Ali in ["3", "03"]:
 			os.system("python FILE-DUMP.py")
-	    if Ali in ["4", "04"]:
-		    os.system("python Huntter404.py")
+		if Ali in ["4", "04"]:
+			os.system("python Huntter404.py")
 		if Ali in ["5", "05"]:
 			self.old()
 		if Ali in ["6", "06"]:
